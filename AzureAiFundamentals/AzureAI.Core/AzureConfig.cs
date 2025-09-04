d8e02943fa50c9b89c0894958764311686e4be61
John Muraski <john.t.muraski@outlook.com>
2025-09-04 18:32:02 -0500

    Add Azure Key Vault integration and new project files
    - Updated `AzureAI.ContentModeration.Core.csproj` and
      `AzureAI.ContentModeration.Text.csproj` to include new
      package references for Azure.Identity,
      Azure.Security.KeyVault.Secrets, and
      Microsoft.Extensions.Configuration, targeting .NET 8.0.
    
    - Created new project file `AzureAI.Core.csproj` with
      similar package references.
    
    - Updated `AzureAiFundamentals.sln` to include new
      projects: `Utility`, `AzureAI.Core`, and `Interfaces`.
    
    - Added `AzureConfig` class in `AzureConfig.cs` to
      implement `IAzureConfig` interface, providing
      properties for `ApiKey` and `ApiEndpoint` and
      methods to retrieve these values from Azure Key Vault
      asynchronously.
    
    - Defined `IAzureConfig` interface in `IAzureConfig.cs`
      to standardize configuration properties.

---
+++ AzureAiFundamentals/AzureAI.Core/AzureConfig.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Security.KeyVault.Secrets;
+
+namespace AzureAI.Core
+{
+    public class AzureConfig : IAzureConfig
+    {
+        // Properties
+        const string keyUrl = "https://kv-azure-fundys-jtm.vault.azure.net/";
+
+        public string ApiKey { get; set; }
+        public string ApiEndpoint { get; set; }
+
+        public static async Task<AzureConfig> GetApiCredentialsAsync(string apiKeySecretName, string apiEndpointSecretName)
+        {
+            var config = new AzureConfig();
+
+            config.ApiKey = await GetSecretFromKeyVaultAsync(apiKeySecretName);
+            config.ApiEndpoint = await GetSecretFromKeyVaultAsync(apiEndpointSecretName);
+
+            return config;
+        }
+
+        private static async Task<string?> GetSecretFromKeyVaultAsync(string apiKeySecretName)
+        {
+            var client = new SecretClient(new Uri(keyUrl), new Azure.Identity.DefaultAzureCredential());
+            return (await client.GetSecretAsync(apiKeySecretName)).Value.Value;
+        }
+    }
+}
---
+++ AzureAiFundamentals/AzureAI.Core/IAzureConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace AzureAI.Core
+{
+    public interface IAzureConfig
+    {
+        string ApiEndpoint { get; set; }
+        string ApiKey { get; set; }
+    }
+}
