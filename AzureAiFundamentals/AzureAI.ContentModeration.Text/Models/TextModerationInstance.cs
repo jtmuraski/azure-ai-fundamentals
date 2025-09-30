76806d60338bbc27ad427062c86f1f2a594a3d94
John Muraski <john.t.muraski@outlook.com>
2025-09-30 06:12:47 -0500

    Update project dependencies and add TextModerationInstance
    - Added package references for Microsoft.Extensions.Configuration.Binder and Microsoft.Extensions.Logging in the project file.
    - Removed folder declarations for Models and Services, indicating a potential restructuring.
    - Introduced a new class `TextModerationInstance` to manage text moderation properties and scores, enhancing the content moderation functionality.

---
+++ AzureAiFundamentals/AzureAI.ContentModeration.Text/Models/TextModerationInstance.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureAI.ContentModeration.Text.Models
+{
+    public class TextModerationInstance
+    {
+        // ---Properties---
+        public string TextToModerate { get => textToModerate; set => textToModerate = value; }
+        public List<string> BlockList { get => blockList; set => blockList = value; }
+        public double HateScore { get => hateScore; set => hateScore = value; }
+        public double SelfHarmScore { get => selfHarmScore; set => selfHarmScore = value; }
+        public double SexualScore { get => sexualScore; set => sexualScore = value; }
+        public double ViolenceScore { get => violenceScore; set => violenceScore = value; }
+
+        // ---Fields---
+        private string textToModerate;
+        private List<string> blockList;
+        private double hateScore;
+        private double selfHarmScore;
+        private double sexualScore;
+        private double violenceScore;
+
+        // ---Constructors---
+        public TextModerationInstance()
+        {
+            textToModerate = string.Empty;
+            blockList = new List<string>();
+            hateScore = 0.0;
+            selfHarmScore = 0.0;
+            sexualScore = 0.0;
+            violenceScore = 0.0;
+        }
+
+
+
+    }
+}
